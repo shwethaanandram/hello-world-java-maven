9ea21c70df6d08b5233d9a98c69daa6c0b981a1b
shwetha <shwetha.na88@gmail.com>
2021-05-04 18:14:34 +0530

    removed system in statement

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -3,8 +3,6 @@ package hello;
 public class Greeter {
     public String sayHello() {
 	System.out.println("Hello india");
-        return "Hello World!!";
-	System.out.println("Hello world");
         return "Hello New World!!";
     }
 }
