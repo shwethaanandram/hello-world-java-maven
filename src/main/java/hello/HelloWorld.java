a8e9f7dee24bfb6d45649550e4e21ead4f1d807f
shwetha <shwetha.na88@gmail.com>
2021-05-04 18:24:07 +0530

    greeter and helloworld java file changed

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -9,8 +9,6 @@ public class HelloWorld {
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
-        System.out.println("Hello INDIA");
-	//This is comment
-	//This is second comment
+        System.out.println("INDIA");
     }
 }
