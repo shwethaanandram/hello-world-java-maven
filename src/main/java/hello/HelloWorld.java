50425f14fa6282821585f37cb8befffdc0f17332
shwetha <shwetha.na88@gmail.com>
2021-05-04 18:53:13 +0530

    resolved conflicts in helloworld java file

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -9,12 +9,7 @@ public class HelloWorld {
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
-<<<<<<< HEAD
-        System.out.println("india");
-	System.out.println("bangalore");
-=======
         System.out.println("INDIA");
 	System.out.println("Bangalore");
->>>>>>> 5c5e1f380ffb8a2301bfa58b261f72c943d9f932
     }
 }
